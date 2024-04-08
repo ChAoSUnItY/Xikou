0164b718b5da9a0d6f7f92efa60f8bcca19dabe1
Kyle Lin <minecraft.kyle.train@gmail.com>
2024-04-08 13:43:51 +0800

    Automatically add idea run task on gradle setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,28 @@
+import org.jetbrains.gradle.ext.*
+import org.jetbrains.gradle.ext.Application
+
 plugins {
     id("java")
+    id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
 }
 
 group = "github.io.chaosunity.xikou"
 version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+        // Azul covers the most platforms for Java 8 toolchains, crucially including MacOS arm64
+        vendor.set(JvmVendorSpec.AZUL)
+    }
+    // Generate sources and javadocs jars when building and publishing
+    withSourcesJar()
+    // withJavadocJar()
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
 
 repositories {
     mavenCentral()
@@ -11,11 +30,38 @@ repositories {
 
 dependencies {
     implementation("org.ow2.asm:asm:9.6")
-    
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+idea {
+    module {
+        inheritOutputDirs = true
+    }
+    project {
+        settings {
+            runConfigurations {
+                add(Application("Compile Self Hosting Compiler", project).apply {
+                    mainClass = "github.io.chaosunity.xikou.Main"
+                    moduleName = "${project.idea.module.name}.main"
+                    programParameters = "self_host/lexer.xk output"
+                })
+                add(Application("Compile Example", project).apply {
+                    mainClass = "github.io.chaosunity.xikou.Main"
+                    moduleName = "${project.idea.module.name}.main"
+                    programParameters = "example/Box.xk output"
+                })
+            }
+            compiler.javac {
+                afterEvaluate {
+                    javacAdditionalOptions = "-encoding utf8"
+                }
+            }
+        }
+    }
 }
